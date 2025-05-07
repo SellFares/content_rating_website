af61ab373abe8272ecc4d8a02eb37de51712b669
brijesh <brijesh.b.gujarati@gmail.com>
2025-05-07 13:21:47 +0100

    Initial commit for like-dislike feature

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,16 +1,33 @@
-
-import React, { Component } from 'react';
-import './ContentRating.css';
+import React, { Component } from "react";
+import "./ContentRating.css";
 
 class ContentRating extends Component {
   constructor() {
-    super();
+    super(),
+      (this.state = {
+        likes: 0,
+        dislike: 0,
+        handleLike: () => {
+          this.setState((prevState) => ({ likes: prevState.likes + 1 }));
+        },
+        handleDisLike: () => {
+          this.setState((prevState) => ({ dislike: prevState.dislike + 1 }));
+        },
+      });
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <div className="content-rating">
+        <p>---Add text here---</p>
+        <div className="rating-buttons">
+          <button className="like-button" onClick={this.state.handleLike}>
+            {this.state.likes}
+          </button>
+          <button className="dislike-button" onClick={this.state.handleDisLike}>
+            {this.state.dislike}
+          </button>
+        </div>
+      </div>
     );
   }
 }

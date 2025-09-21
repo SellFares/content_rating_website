96caed540606c572114ce55430c18b49feaa4481
Ashlyn Alama <ashlynbotin@gmail.com>
2025-09-21 19:47:24 +0800

    Updated ContentRating.jsx

--- content_rating-like-and-dislike-/src/Components/ContentRating.jsx
+++ content_rating-like-and-dislike-/src/Components/ContentRating.jsx
@@ -5,14 +5,43 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+     this.state = {
+      likes: 0,
+      dislikes: 0
+    };
   }
+  handleLike = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1
+    }));
+  };
+  handleDislike = () => {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1
+    }));
+  };
+
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className="content-rating">
+        <p>
+         Lorem ipsum shit dolor sit amet consectetur adipisicing elit. Sint delectus eos ad magnam debitis quaerat et illum magni, laudantium harum odio assumenda
+         dicta consequatur fugit voluptates tempora molestiae rerum quibusdam?
+        </p>
+        <div className="rating-buttons"> 
+          <button className="like-button" onClick={this.handleLike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dislike-button" onClick={this.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+        </div>
+      </div>
      </>
     );
   }
 }
 
 export default ContentRating;
+

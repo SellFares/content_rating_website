ceac87cfa12ac0708fa433a8363d9e0eef71a518
SellFares <SWE2409146@xmu.edu.my>
2025-10-15 13:24:17 -0400

    message

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -7,21 +7,34 @@ class ContentRating extends Component {
     super();
     this.state = {
         likes : 0 ,
-        dislikes : 0 
+        dislikes : 0 ,
+
+        handleLike:() => {
+            this.setState( (prevState) => ({
+                likes : prevState.likes + 1
+            }) );
+        },
+
+        handleDislike:() => {
+            this.setState( (prevState) => ({
+                dislikes : prevState.dislikes + 1
+            }) );
+        }
     };
+
   }
   render() {
     return (
      <>
         <div className='content-rating'>
             <p>
-                //Add text here
+                zid w cha3lik
             </p>
             <div className='rating-buttons'>
-                <button className='like-button'>
+                <button className='like-button' onClick={this.state.handleLike}>
                     Like ({this.state.likes})
                 </button>
-                <button className='dislike-button'>
+                <button className='dislike-button' onClick={this.state.handleDislike}>
                     Dislike ({this.state.dislikes})
                 </button>
             </div>
